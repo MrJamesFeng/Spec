0229606b4fb07cacbac0fb12f4e778aa72321cf3
丰静 <812463918@qq.com>
2017-03-13 22:11:19 +0800

    初始化

---
+++ test Spec/test Spec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test Spec
+//
+//  Created by 丰静 on 2017/3/13.
+//  Copyright © 2017年 丰静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test Spec/test Spec/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test Spec
+//
+//  Created by 丰静 on 2017/3/13.
+//  Copyright © 2017年 丰静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
